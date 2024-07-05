dcbc4250d5bde8a9ab967caed198fe51d5785367
Sudhakar <40350467+SudSwat@users.noreply.github.com>
2024-07-05 12:35:45 +0530

    Add files via upload

---
+++ solomon_reeborg.py
@@ -0,0 +1,135 @@
+
+logo Reeborg's World
+
+ 
+10/10
+
+    Python Code
+    library
+
+def turn_right():
+
+    turn_left()
+
+    turn_left()
+
+    turn_left()
+
+    
+
+def step1():
+
+    move()
+
+    move()
+
+ 
+
+def big_step():
+
+    step1()
+
+    turn_right()
+
+    step1()
+
+    turn_left()
+
+    step1()
+
+    turn_left()
+
+    step1()
+
+    turn_right()
+
+    step1()
+
+​
+
+def s_steps():
+
+    turn_right()
+
+    step1()
+
+    turn_left()
+
+    step1()
+
+    turn_left()
+
+    step1()
+
+    turn_right()
+
+    step1()
+
+    
+
+​
+
+​
+
+step1()
+
+turn_left()
+
+move()
+
+turn_right()
+
+step1()
+
+​
+
+for stepping in range(4):
+
+    s_steps()
+
+​
+
+​
+
+turn_right()
+
+step1()
+
+turn_left()
+
+step1()
+
+move()
+
+​
+
+​
+
+​
+
+​
+
+​
+
+​
+
+    
+
+ 
+
+​
+
+​
+
+​
+
+​
+
+​
+
+​
+
+    
+
+    
+
